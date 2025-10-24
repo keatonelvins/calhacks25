14386636d4014fd8b05989e8f6c372e2c031adca
k <keatone@gmail.com>
2025-10-23 23:15:48 -0700

    use full node and reduce mbs

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 - Make an account at https://modal.com, then run:
 ```bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv sync
 uv run modal setup
 uv run modal create wandb-secret WANDB_API_KEY=...
 ```
 
-# Run:
+# Train:
 ```bash
 uv run modal run train.py
 uv run modal run --detach train.py  # or detach for long runs
 ```
+
+# Refs
+- https://github.com/PrimeIntellect-ai/verifiers/tree/main/verifiers/rl
--- train.py
+++ train.py
@@ -2,30 +2,30 @@ import modal
 import subprocess
 from pathlib import Path
 
-CONFIG = "wordle.toml"
-NUM_GPUS = 2
-
+config = "wordle.toml"
 app = modal.App("vf-wordle")
+artifacts_volume = modal.Volume.from_name("vf-wordle-artifacts", create_if_missing=True)
 
 image = (
     modal.Image.from_registry("nvidia/cuda:12.8.1-devel-ubuntu24.04", add_python="3.12")
     .apt_install("git")
     .pip_install("uv")
-    .uv_sync(extras=["verifiers[rl]"])
+    .uv_sync(extras=["rl"])
     .uv_pip_install("wordle", extra_index_url="https://hub.primeintellect.ai/will/simple/")
-    .add_local_file("wordle.toml", f"/root/{CONFIG}")
+    .add_local_file(config, f"/root/{config}")
 )
 
 @app.function(
     image=image,
-    gpu=f"H100:{NUM_GPUS}",
-    secrets=[modal.Secret.from_name("wandb-secret")],
+    gpu="H100:8",
     timeout=60 * 60 * 24,
+    volumes={"/artifacts": artifacts_volume},
+    secrets=[modal.Secret.from_name("wandb-secret")],
 )
 def train():
     from verifiers.scripts.rl import load_toml, build_vllm_command
 
-    data = load_toml(Path(f"/root/{CONFIG}"))
+    data = load_toml(Path(f"/root/{config}"))
     infer_gpus = data["inference"]["gpus"]
     total_gpus = infer_gpus + data["trainer"]["gpus"]
     
@@ -33,7 +33,7 @@ def train():
     trainer_gpu_str = "CUDA_VISIBLE_DEVICES=" + ",".join(str(i) for i in range(infer_gpus, total_gpus))
     
     vllm_cmd = build_vllm_command(data["model"], data["inference"], inference_gpu_str)
-    train_cmd = " ".join([trainer_gpu_str, "uv run", "vf-train", "@", f"/root/{CONFIG}"])
+    train_cmd = " ".join([trainer_gpu_str, "uv run", "vf-train", "@", f"/root/{config}"])
 
     subprocess.Popen(vllm_cmd, shell=True)
     subprocess.run(train_cmd, shell=True)
